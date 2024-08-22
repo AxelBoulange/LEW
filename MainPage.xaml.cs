c526d783c8574584515ca6e728a76953837235ae
Ypo <121876885+AxelBoulange@users.noreply.github.com>
2024-08-22 10:50:21 +0200

    Custom Colors in StaticRessources

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,18 +5,9 @@ namespace LEW
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-            test();
-        }
-
-        private async void test()
-        {
-            var test = await Translate.EnglishToFrench("something");
-            var test2 = await RandomWord.RandomEnglish(5);
         }
     } 
 }
